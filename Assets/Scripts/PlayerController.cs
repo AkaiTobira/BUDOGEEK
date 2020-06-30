e05cc76124dfb46fe71e90e2129121d083f15899
AdvmS <adamsad99@interia.com>
2020-06-30 22:01:09 +0200

    rebuilding whole game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,80 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public int hitPoints = 3;
-    public int score = 0;
-    private new Rigidbody2D rigidbody;
     private Animator playerAnimation;
-    private Animation playerAnimation2;
-    public Vector3 respawnPoint;
-    public LevelManager gameLevelManager;
+    public int button;
+    public int hitPoints = 3;
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
         playerAnimation = GetComponent<Animator>();
-        respawnPoint = transform.position;
-        gameLevelManager = FindObjectOfType<LevelManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
+        //
+        if (Input.GetKeyDown("w"))
         {
-            playerAnimation.SetTrigger("");
-        }
-        //Obracanie postaci w przypadku ataku w lewą/prawą stronę
-        
-        //if (/*left side is clicked*/)
-        //{
-         //   transform.localScale = new Vector2(1,1);
-        //}
-        //if (/*right side is clicked*/)
-        //{
-        //    transform.localScale = new Vector2(-1, 1);
-        //}
-        //KeyCode key = Ger;
-        //isClicked = Input.GetKeyDown("LeftArrow");
-        //playerAnimation.SetBool("OnClick", isClicked);
-
-    }
-    
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-        //if (playerAnimation.tag == "StandingAnimation")
-        //{
-            if (hitPoints == 3 || hitPoints == 2)
+            if (playerAnimation.GetCurrentAnimatorStateInfo(0).ToString() == "standing")
             {
-                //playerAnimation.tag = "GettingHitAnimation";
-                //playerAnimation.Play(playerAnimation.tag == "GettingHitAnimation"));
-                playerAnimation.SetTrigger("GettingHit");
-                Debug.Log("Getting hit, HP decreases...");
-                hitPoints--;
-                Debug.Log("HP: " + hitPoints);
-                playerAnimation.SetTrigger("HasGotHit");
+                if (hitPoints == 3 || hitPoints == 2)
+                {
+                    //playerAnimation.tag = "GettingHitAnimation";
+                    //playerAnimation.Play(playerAnimation.tag == "GettingHitAnimation"));
+                    playerAnimation.SetTrigger("GetHit");
+                    Debug.Log("Getting hit, HP decreases...");
+                    hitPoints--;
+                    Debug.Log("HP: " + hitPoints);
+                    playerAnimation.SetTrigger("HasGotHit");
+                }
+                else if (hitPoints == 1)
+                {
+                    playerAnimation.SetTrigger("GettingLastHit");
+                    Debug.Log("Getting hit, HP decreases...");
+                    hitPoints--;
+                    Debug.Log("HP: " + hitPoints);
+                    //playerAnimation.SetTrigger("HasBeenKilled");
+                    //gameLevelManager.Respawn();
+                }
             }
-            else if (hitPoints == 1)
-            {
-                playerAnimation.SetTrigger("GettingLastHit");
-                Debug.Log("Getting hit, HP decreases...");
-                hitPoints--;
-                Debug.Log("HP: " + hitPoints);
-                playerAnimation.SetTrigger("HasBeenKilled");
-                gameLevelManager.Respawn();
-            }
-
-        //}
-        /*
-        else
-        {
-            Debug.Log("Your score increases.");
-            score++;
-            Debug.Log("Score: " + score);
+            playerAnimation.SetTrigger("OnTap");
         }
-        */
     }
-
 }
